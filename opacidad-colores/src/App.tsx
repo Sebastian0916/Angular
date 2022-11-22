567705b904165f0a9797b53c6ea2c311972bd859
Sebastian0916 <Sebas.vera0916@gmail.com>
2022-11-22 08:22:58 -0500

    Avance

--- opacidad-colores/src/App.tsx
+++ opacidad-colores/src/App.tsx
@@ -1,18 +1,12 @@
-import React from "react";
-import { Stack } from "@mui/system";
-import { Button } from "@mui/material";
-import "./App.css";
+
+import React from 'react'
 
 function App() {
   return (
-    <>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
